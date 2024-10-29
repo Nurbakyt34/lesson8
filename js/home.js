835ee1b8128aeb4109647cbfe8b34b0f80c14fb1
Nurbakyt <n79907561@gmail.com>
2024-10-29 14:36:42 +0600

    lesson8

--- js/home.js
+++ js/home.js
@@ -1,44 +1,41 @@
 const API = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=' 
  
-const menu = [ 
-    { 
-        img: "br-fest.svg", 
-        name: "breakfest" 
-    }, 
-    { 
-        img: "icon2.svg", 
-        name: "lunch" 
-    }, 
-    { 
-        img: "br-fest1", 
-        name: "dinner" 
-    }, 
-    { 
-        img: "icon-4.svg", 
-        name: "dessert" 
-    }, 
-    { 
-        img: "icon-5.svg", 
-        name: "quick bite!" 
-    }, 
-] 
- 
+
+const menu = [
+    {
+        img: 'br-fest.svg',
+        name: 'breakfest',
+    },
+
+    {
+        img: 'lunch.svg',
+        name: 'lunch',
+    },
+
+    {
+        img: 'dinner.svg',
+        name: 'dinner',
+    },
+
+    {
+        img: 'dessert.svg',
+        name: 'dessert',
+    },
+
+    {
+        img: 'bite.svg',
+        name: 'quick bite!',
+    },
+
+   
+]
+
 const menuEL = document.getElementById("menu") 
  
 menuEL.innerHTML = menu.map(m => { 
     return ` 
             <div> 
-                <img src="../img/${m.img}" alt=""> 
+                <img src="../images/${m.img}" alt=""> 
                 <h4>${m.name}</h4> 
             </div>` 
-}).join('') 
- 
-function getMeals(categoty = "Starter"){ 
-    fetch(API + categoty) 
-    .then( response => response.json()) 
-    .then( foods => { 
-        console.log(foods); 
-    }) 
-} 
- 
-getMeals()
+}).join('')
